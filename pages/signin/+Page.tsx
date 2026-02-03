92bb088b1b8815be5e4baac2993719b2ae9fdb51
Alexandre Morozoff <morozoff_alexandre@outlook.com>
2026-02-03 14:19:29 +0100

    commit to clean push conflict

--- pages/signin/+Page.tsx
+++ pages/signin/+Page.tsx
@@ -1,20 +1,22 @@
-import { Link } from "../../components/Link"
+import { Link } from "@/components/Link";
 
-export default function login(){
-  return(
-    <div >
+export default function login() {
+  return (
+    <div>
       <div className="flex flex-col">
         <h1>Login</h1>
         <label htmlFor="username">Identifiant :</label>
         <input type="text" id="username" className=" border-2 m-1" />
         <label htmlFor="password">Mot de passe :</label>
         <input type="text" id="username" className=" border-2 m-1" />
-        <button type="submit" className="border-1 mt-2">Envoyer</button>
+        <button type="submit" className="border-2 mt-2">
+          Envoyer
+        </button>
       </div>
       <div className="mt-4">
         <h3>Pas encore inscrit ?</h3>
         <Link href="/signup">Inscription</Link>
       </div>
     </div>
-  )
+  );
 }
