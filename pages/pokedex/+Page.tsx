9d4d27dcaea999b952f0da6bbbd55376bcc18c58
BeauclairN <beauclair.nat@gmail.com>
2026-02-03 15:44:47 +0100

    Add lazyloading to pokedex page

--- pages/pokedex/+Page.tsx
+++ pages/pokedex/+Page.tsx
@@ -1,9 +1,17 @@
+import { lazy, Suspense } from "react";
+
+//const Component = lazy(() => import(".///"))
+
 export default function Pokedex() {
     return (
         <main>
             <h1>Pokédex</h1>
 
-            
+            <div className={"bg-orange-800"}>
+                <Suspense fallback={<p>Chargement</p>}>
+                    {/* <Component /> */}
+                </Suspense>
+            </div>
         </main>
     );
 }
