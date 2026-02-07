81beb7e0b61465114ae933d521d79d8d9f9d4b94
Alexandre Morozoff <morozoff_alexandre@outlook.com>
2026-02-07 15:40:21 +0100

    fix: the landing page has no scrollbar anymore

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,11 @@
 export function Nav() {
   return (
-    <nav className="flex justify-between p-5 bg-amber-100 max-w-screen">
+    <nav className="flex justify-between p-5 bg-amber-100 w-full">
       <div className="flex gap-5">
         <a href="/">Accueil</a>
-        <a href="/pokedex">Pokedex</a> 
-        <a href="/team">Equipe</a> 
-        <a href="/combat">Combat</a> 
+        <a href="/pokedex">Pokedex</a>
+        <a href="/team">Equipe</a>
+        <a href="/combat">Combat</a>
         <a href="/leaderboard">Classement</a>
       </div>
       <div className="flex gap-5">
--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,29 +1,31 @@
-import backgoundPkmn from "@/assets/backgroundEmrld.png";
+import backgroundPkmn from "@/assets/backgroundEmrld.webp";
 
 export default function Page() {
   return (
-    <div className="bg-[url(@/assets/backgroundEmrld.webp)] min-h-screen min-w-dvw flex flex-col items-center gap-20 justify-center">
-      <div>
-        <img
-          className="w-180 h-60 "
-          src="../assets/pokeventureTitle.webp"
-          alt="PokeVenture"
-        ></img>
-      </div>
-      <div className="flex gap-8 ">
+    <main
+      className="w-full min-h-full flex flex-col items-center justify-center gap-8 bg-center bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${backgroundPkmn})` }}
+    >
+      <img
+        className="max-w-3xl h-auto"
+        src="../assets/pokeventureTitle.webp"
+        alt="PokeVenture"
+      />
+
+      <div className="flex gap-8">
         <a
-          className="bg-green-300 from-green-300 to-green-100 to border-3 border-gray-600 hover:border-orange-500 p-5 rounded-xl "
+          className="bg-green-300 border-2 border-gray-600 hover:border-orange-500 p-5 rounded-xl"
           href="/signin"
         >
           Se connecter
         </a>
         <a
-          className="bg-green-300 border-3 border-gray-600 hover:border-orange-500 p-5 rounded-xl"
+          className="bg-green-300 border-2 border-gray-600 hover:border-orange-500 p-5 rounded-xl"
           href="/signup"
         >
           S'inscrire
         </a>
       </div>
-    </div>
+    </main>
   );
 }
