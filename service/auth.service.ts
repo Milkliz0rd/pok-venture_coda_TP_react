ddbd8169ab71286d421ac2fc265a1314f6e6ebbc
Alexandre Morozoff <morozoff_alexandre@outlook.com>
2026-02-07 17:58:37 +0100

    init auth.service.ts

---
+++ service/auth.service.ts
@@ -0,0 +1,48 @@
+import argon2 from "argon2";
+import * as jose from "jose";
+import { getPrisma } from "@/utils/getPrisma";
+
+const secret = jose.base64url.decode(
+  "secret_zH4NRP1HMALxxCFnRZABFA7GOJtzU_gIj02a",
+);
+
+export const authService = {
+  // Inscription
+  async register(username: string, email: string, password: string) {
+    const prisma = getPrisma();
+    const hashedPassword = await argon2.hash(password);
+
+    await prisma.user.create({
+      data: { username, email, password: hashedPassword },
+    });
+
+    prisma.$disconnect();
+  },
+
+  // Connexion
+  async login(email: string, password: string) {
+    const prisma = getPrisma();
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user) throw new Error("User not found");
+
+    const isPasswordValid = await argon2.verify(user.password, password);
+    if (!isPasswordValid) throw new Error("Invalid password");
+
+    prisma.$disconnect();
+    return this.generateJWT(user.id.toString());
+  },
+
+  // Générer un JWT
+  async generateJWT(userId: string) {
+    return new jose.EncryptJWT({ id: userId })
+      .setProtectedHeader({ alg: "dir", enc: "A128CBC-HS256" })
+      .encrypt(secret);
+  },
+
+  // Vérifier un JWT
+  async verifyJWT(token: string) {
+    const { payload } = await jose.jwtVerify(token, secret);
+    return payload as { id: string };
+  },
+};
