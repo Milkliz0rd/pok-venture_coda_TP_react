7ff4bbaf735e5fcccb0427ec21a80233bd4ace63
Alexandre Morozoff <morozoff_alexandre@outlook.com>
2026-02-07 18:04:20 +0100

    init cookie-parser on entry.ts && telefunc.ts init

--- service/auth.service.ts
+++ service/auth.service.ts
@@ -7,40 +7,31 @@ const secret = jose.base64url.decode(
 );
 
 export const authService = {
-  // Inscription
   async register(username: string, email: string, password: string) {
     const prisma = getPrisma();
     const hashedPassword = await argon2.hash(password);
-
     await prisma.user.create({
       data: { username, email, password: hashedPassword },
     });
-
     prisma.$disconnect();
   },
 
-  // Connexion
   async login(email: string, password: string) {
     const prisma = getPrisma();
     const user = await prisma.user.findUnique({ where: { email } });
-
     if (!user) throw new Error("User not found");
-
     const isPasswordValid = await argon2.verify(user.password, password);
     if (!isPasswordValid) throw new Error("Invalid password");
-
     prisma.$disconnect();
     return this.generateJWT(user.id.toString());
   },
 
-  // Générer un JWT
   async generateJWT(userId: string) {
     return new jose.EncryptJWT({ id: userId })
       .setProtectedHeader({ alg: "dir", enc: "A128CBC-HS256" })
       .encrypt(secret);
   },
 
-  // Vérifier un JWT
   async verifyJWT(token: string) {
     const { payload } = await jose.jwtVerify(token, secret);
     return payload as { id: string };
